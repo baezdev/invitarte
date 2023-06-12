f6ff5218950d9a813504c1312dd5aad146d52b71
baezdev <baezdev@gmail.com>
2023-06-12 00:39:39 -0600

    add interfaces

---
+++ src/interfaces/interfaces.d.ts
@@ -0,0 +1,22 @@
+export interface Testimonial {
+  name: string;
+  message: string;
+  rating: number;
+}
+
+export interface InvitationContent {
+  title: string;
+  desc: string;
+  icon: string;
+}
+
+export interface About {
+  id: string;
+  title: string;
+  desc: string;
+}
+
+export interface Event {
+  eventName: string;
+  image: string;
+}
