c4605f966e7a05cfdce81a94d694fd20d3a3c24b
baezdev <baezdev@gmail.com>
2023-06-13 22:45:35 -0600

    styles: change link padding

--- src/components/ui/NavLinks.tsx
+++ src/components/ui/NavLinks.tsx
@@ -40,7 +40,7 @@ export const NavLinks = () => {
         {links.map(({ text, href }) => (
           <li key={text}>
             <a
-              className="px-1 py-px relative after:content-[''] after:absolute  after:w-0 after:hover:w-full after:h-[3px] after:bg-black after:left-0 after:bottom-0 after:rounded after:transition-all after:duration-300"
+              className="px-1 py-[2.5px] relative after:content-[''] after:absolute after:w-0 after:hover:w-full after:h-[3px] after:bg-black after:left-0 after:bottom-0 after:rounded after:transition-all after:duration-300"
               href={href}
             >
               {text}
