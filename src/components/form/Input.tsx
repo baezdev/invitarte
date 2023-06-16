dd3f984a4b61d5995e572bbd991676dc5244736b
baezdev <baezdev@gmail.com>
2023-06-16 13:05:19 -0600

    add register custom input

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import Image from "next/image";
-import { useState } from "react";
+import { useState } from "react"
+import Image from "next/image"
+import { useFormContext } from "react-hook-form"
 
 interface Props {
   name: string;
@@ -14,6 +15,7 @@ interface Props {
 
 export const Input = ({ name, label, type, holder, icon, done }: Props) => {
   const [inputType, setInputType] = useState(type);
+  const { register } = useFormContext()
 
   function showPassword() {
     inputType === "password" ? setInputType("text") : setInputType("password");
@@ -21,12 +23,13 @@ export const Input = ({ name, label, type, holder, icon, done }: Props) => {
 
   return (
     <>
-      <label htmlFor={name} className="block mb-3 font-semibold">
+      <label htmlFor={name} className="block mb-2 font-semibold">
         {label}
       </label>
       <div className="flex justify-between gap-x-3 py-4 md:py-3 px-4 border-2 border-gray-200 focus-within:border-primary bg-gray-50 rounded-xl items-center [&>svg]:focus-within:fill-primary focus-within:bg-white transition-all duration-200 w-full">
         {icon}
         <input
+          {...register(name)}
           type={inputType}
           className="flex-1 w-32 text-lg font-medium bg-transparent outline-none"
           placeholder={holder}
