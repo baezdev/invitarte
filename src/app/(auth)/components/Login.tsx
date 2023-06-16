007a946b78c46e3c91fe3a54c14625c0a84392f3
baezdev <baezdev@gmail.com>
2023-06-16 13:04:57 -0600

    Add form provider

--- src/app/(auth)/components/Login.tsx
+++ src/app/(auth)/components/Login.tsx
@@ -1,34 +1,50 @@
-import { Input } from "../../../components/form/Input"
-import { Button } from "../../../components/ui/Button"
-import { Email, Lock } from "../../../components/ui/Icons"
+'use client'
 
+import { Input } from "@/components/form/Input"
+import { Button } from "@/components/ui/Button"
+import { Email, Lock } from "@/components/ui/Icons"
+import { useForm, FormProvider, type SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+  email: string,
+  password: string
+}
 
 export const Login = () => {
+  const formMethods = useForm<Inputs>()
+  const { handleSubmit } = formMethods
+
+  const formSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
+
   return (
-    <form className="mt-5">
-      <div className="mb-5">
-        <Input
-          name="email"
-          type="email"
-          label="Email o Número de teléfono"
-          holder="ejemplo@correo.com"
-          icon={<Email />}
-          done
-        />
-      </div>
-      <div className="mb-5">
-        <Input
-          name="password"
-          type="password"
-          label="Contraseña"
-          holder="Minimo 8 caracteres"
-          icon={<Lock />}
-          done
-        />
-      </div>
-      <div className="mt-9">
-        <Button>Acceder</Button>
-      </div>
-    </form>
+    <FormProvider {...formMethods}>
+      <form onSubmit={handleSubmit(formSubmit)} className="mt-5">
+        <div className="mb-5">
+          <Input
+            name="email"
+            type="email"
+            label="Email o Número de teléfono"
+            holder="ejemplo@correo.com"
+            icon={<Email />}
+            done
+          />
+        </div>
+        <div className="mb-5">
+          <Input
+            name="password"
+            type="password"
+            label="Contraseña"
+            holder="Minimo 8 caracteres"
+            icon={<Lock />}
+            done
+          />
+        </div>
+        <div className="mt-9">
+          <Button>Acceder</Button>
+        </div>
+      </form>
+    </FormProvider>
   )
 }
--- src/app/(auth)/components/Register.tsx
+++ src/app/(auth)/components/Register.tsx
@@ -1,44 +1,51 @@
+'use client'
+
+import { useForm, FormProvider } from "react-hook-form"
 import { Input } from "@/components/form/Input";
 import { Button } from "@/components/ui/Button";
 import { Email, Lock, User } from "@/components/ui/Icons";
 
 
 export const Register = () => {
+  const formMethods = useForm()
+
   return (
-    <form className="mt-5">
-      <div className="mb-5">
-        <Input
-          name="name"
-          type="text"
-          label="Nombre"
-          holder="Su nombre"
-          icon={<User />}
-          done
-        />
-      </div>
-      <div className="mb-5">
-        <Input
-          name="email"
-          type="email"
-          label="Email o Número de teléfono"
-          holder="ejemplo@correo.com"
-          icon={<Email />}
-          done
-        />
-      </div>
-      <div className="mb-5">
-        <Input
-          name="password"
-          type="password"
-          label="Contraseña"
-          holder="Minimo 8 caracteres"
-          icon={<Lock />}
-          done
-        />
-      </div>
-      <div className="mt-9">
-        <Button>Registrate</Button>
-      </div>
-    </form>
+    <FormProvider {...formMethods}>
+      <form className="mt-5">
+        <div className="mb-5">
+          <Input
+            name="name"
+            type="text"
+            label="Nombre"
+            holder="Su nombre"
+            icon={<User />}
+            done
+          />
+        </div>
+        <div className="mb-5">
+          <Input
+            name="email"
+            type="email"
+            label="Email o Número de teléfono"
+            holder="ejemplo@correo.com"
+            icon={<Email />}
+            done
+          />
+        </div>
+        <div className="mb-5">
+          <Input
+            name="password"
+            type="password"
+            label="Contraseña"
+            holder="Minimo 8 caracteres"
+            icon={<Lock />}
+            done
+          />
+        </div>
+        <div className="mt-9">
+          <Button>Registrate</Button>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
