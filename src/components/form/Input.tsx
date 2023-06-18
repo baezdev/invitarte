253ca045c9fdeeb21de3de2612cde1e1e5332bfc
baezdev <baezdev@gmail.com>
2023-06-18 12:58:44 -0600

    add formik field

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -2,23 +2,25 @@
 
 import { useState } from "react"
 import Image from "next/image"
-import { useFormContext } from "react-hook-form"
+import { ErrorMessage, Field } from "formik"
 
-interface Props {
+interface InputProps {
   name: string;
   label: string;
   type: string;
   holder: string;
   icon: JSX.Element;
   done?: boolean;
+  validation?: any
 }
 
-export const Input = ({ name, label, type, holder, icon, done }: Props) => {
-  const [inputType, setInputType] = useState(type);
-  const { register } = useFormContext()
+export const Input = ({ ...props }) => {
+  const [showPassword, setShowPassword] = useState(false)
 
-  function showPassword() {
-    inputType === "password" ? setInputType("text") : setInputType("password");
+  const { name, label, type, holder, icon, done } = props
+
+  const switchShowPassword = () => {
+    setShowPassword(!showPassword)
   }
 
   return (
@@ -28,20 +30,21 @@ export const Input = ({ name, label, type, holder, icon, done }: Props) => {
       </label>
       <div className="flex justify-between gap-x-3 py-4 md:py-3 px-4 border-2 border-gray-200 focus-within:border-primary bg-gray-50 rounded-xl items-center [&>svg]:focus-within:fill-primary focus-within:bg-white transition-all duration-200 w-full">
         {icon}
-        <input
-          {...register(name)}
-          type={inputType}
-          className="flex-1 w-32 text-lg font-medium bg-transparent outline-none"
+        <Field
+          {...props}
+          name={name}
+          type={showPassword ? 'text' : type}
           placeholder={holder}
+          className="flex-1 w-32 text-lg font-medium bg-transparent outline-none"
         />
         <div className="flex items-center gap-3">
           {type === "password" && (
             <button
               className={`px-4 py-1 font-semibold border rounded-lg text-xs text-primary border-primary bg-primary bg-opacity-10 ${
-                inputType !== "password" && "line-through"
+                showPassword && "line-through"
               }`}
               type="button"
-              onClick={showPassword}
+              onClick={switchShowPassword}
             >
               Ver
             </button>
@@ -49,6 +52,15 @@ export const Input = ({ name, label, type, holder, icon, done }: Props) => {
           {done && <Image src="/icons/check.svg" alt="done" width={24} height={24} />}
         </div>
       </div>
+      <ErrorMessage name={name}>
+        {(msg: string) => (
+          <span 
+            className='block mt-1 font-semibold text-red-500'
+          >
+            {msg}
+          </span>
+        )}
+      </ErrorMessage>
     </>
   );
 };
