3792b9f3f491402c397bcbd9bb5fd311e9f8aa0d
baezdev <baezdev@gmail.com>
2023-06-18 12:55:43 -0600

    add env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    url: "https://dnbemjresiainblckltq.supabase.co",
+    apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuYmVtanJlc2lhaW5ibGNrbHRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY5Mzk3NzEsImV4cCI6MjAwMjUxNTc3MX0.NoExKP9w6tWYozqAWy_L6x-w7GMgitUHyitT6lYrHTg"
+  }
+}
 
 module.exports = nextConfig
