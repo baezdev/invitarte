5c0161a790ba50d9e9b901a43fbd7a2957bd0e16
baezdev <baezdev@gmail.com>
2023-06-18 12:59:30 -0600

    add formik components

--- src/app/(auth)/components/Login.tsx
+++ src/app/(auth)/components/Login.tsx
@@ -3,24 +3,27 @@
 import { Input } from "@/components/form/Input"
 import { Button } from "@/components/ui/Button"
 import { Email, Lock } from "@/components/ui/Icons"
-import { useForm, FormProvider, type SubmitHandler } from "react-hook-form"
-
-type Inputs = {
+import { Formik, Form } from 'formik'
+ 
+/* type Inputs = {
   email: string,
   password: string
+} */
+
+const initialValues = {
+  email: '',
+  password: ''
 }
 
 export const Login = () => {
-  const formMethods = useForm<Inputs>()
-  const { handleSubmit } = formMethods
-
-  const formSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data)
-  }
-
   return (
-    <FormProvider {...formMethods}>
-      <form onSubmit={handleSubmit(formSubmit)} className="mt-5">
+    <Formik
+      initialValues={initialValues}  
+      onSubmit={(values, actions) => {
+        console.log(values)
+      }}
+    >
+      <Form className="mt-5">
         <div className="mb-5">
           <Input
             name="email"
@@ -28,6 +31,7 @@ export const Login = () => {
             label="Email o Número de teléfono"
             holder="ejemplo@correo.com"
             icon={<Email />}
+            validation={{ required: true, }}
             done
           />
         </div>
@@ -44,7 +48,7 @@ export const Login = () => {
         <div className="mt-9">
           <Button>Acceder</Button>
         </div>
-      </form>
-    </FormProvider>
+      </Form>
+    </Formik>
   )
 }
