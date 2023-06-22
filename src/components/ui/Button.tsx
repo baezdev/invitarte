d14db44573bf46d690c77414b17bce031feeb405
baezdev <baezdev@gmail.com>
2023-06-22 01:04:48 -0600

    add onClick options

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,13 +1,15 @@
 interface Props {
   children: string | React.ReactNode
   type?: 'button' | 'submit'
+  onClick?: () => void
 }
 
-export const Button = ({ children, type = 'button' }: Props) => {
+export const Button = ({ children, type = 'button', onClick }: Props) => {
   return (
-    <button 
+    <button
       className="bg-primary w-full leading-none inline-block font-semibold text-white px-6 py-[23px] md:py-4 rounded-3xl transition ease-in-out hover:scale-105 duration-300"
       type={type}
+      onClick={onClick}
     >
       {children}
     </button>
