961b693a0d0f742af8e60843276b10803b889acc
baezdev <baezdev@gmail.com>
2023-06-22 01:03:52 -0600

    add remote images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   env: {
     url: "https://dnbemjresiainblckltq.supabase.co",
     apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuYmVtanJlc2lhaW5ibGNrbHRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY5Mzk3NzEsImV4cCI6MjAwMjUxNTc3MX0.NoExKP9w6tWYozqAWy_L6x-w7GMgitUHyitT6lYrHTg"
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      }
+    ]
   }
 }
 
